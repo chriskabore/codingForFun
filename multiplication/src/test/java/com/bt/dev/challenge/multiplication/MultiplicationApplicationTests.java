6b82499e7ad6807c1fe371d94e3dac0ca5d01298
Christian Kabore <chriskabor@gmail.com>
2022-04-13 18:27:03 +0000

    multiplication folder commit

---
+++ multiplication/src/test/java/com/bt/dev/challenge/multiplication/MultiplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bt.dev.challenge.multiplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
